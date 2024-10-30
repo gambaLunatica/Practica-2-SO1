1ec17273a974ad61dd982f35290844de93166dde
Juana Luna <juanaluna553@gmail.com>
2024-10-30 15:30:45 +0100

    Tercer metodo parece que guay

--- Nivel1.c
+++ Nivel1.c
@@ -1,13 +1,15 @@
-//librerias a usar
+#define _POSIX_C_SOURCE 200112L
+// librerias a usar
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
 #include <unistd.h>
-//definir promp
+#include <linux/limits.h>
+// definir promp
 #define PROMPT '$'
 
-//definir colores
+// definir colores
 #define RESET "\033[0m"
 #define NEGRO_T "\x1b[30m"
 #define NEGRO_F "\x1b[40m"
@@ -21,7 +23,7 @@
 #define BLANCO_T "\x1b[97m"
 #define NEGRITA "\x1b[1m"
 
-//definición funciones
+// definición funciones
 void imprimir_prompt();
 char *read_line(char *line);
 int execute_line(char *line);
@@ -34,54 +36,107 @@ int internal_jobs(char **args);
 int internal_fg(char **args);
 int internal_bg(char **args);
 
-//Variable para leer una line
-#define lineSize 1024
-
-int main(){
-    //array para almacenar la linea
-    char line [lineSize];
-    //bucle infinito
-    while(1){
+// Variable para leer una line
+#define COMMAND_LINE_SIZE 1024
+#define ARGS_SIZE 64
+int main()
+{
+    // array para almacenar la linea
+    char line[COMMAND_LINE_SIZE];
+    // bucle infinito
+    while (1)
+    {
         read_line(line);
     }
 }
-void imprimir_prompt() {
-    char *user = getenv("USER");               // Obtener el usuario
-    if (user == NULL) {
-        user = "usuario";                      // Asignar valor predeterminado si es NULL
+void imprimir_prompt()
+{
+    char *user = getenv("USER"); // Obtener el usuario
+    if (user == NULL)
+    {
+        user = "usuario"; // Asignar valor predeterminado si es NULL
     }
-    
+
     char cwd[PATH_MAX];
-    
+
     // Si obtenemos bien el directorio
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf(CYAN_T NEGRITA "%s:" RESET, user);          // Imprimir Usuario 
-        printf(MAGENTA_T NEGRITA "%s " RESET, cwd);       // Imprimir Directorio actual 
-        printf(MAGENTA_T NEGRITA "%c " RESET, PROMPT);    // Imprimir Símbolo PROMPT 
-    } else {
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        printf(CYAN_T NEGRITA "%s:" RESET, user);      // Imprimir Usuario
+        printf(MAGENTA_T NEGRITA "%s " RESET, cwd);    // Imprimir Directorio actual
+        printf(MAGENTA_T NEGRITA "%c " RESET, PROMPT); // Imprimir Símbolo PROMPT
+    }
+    else
+    {
         perror("Error");
     }
 
-    fflush(stdout);  // Forzar el vaciado del buffer de salida
+    fflush(stdout); // Forzar el vaciado del buffer de salida
 }
-char *read_line(char *line){
+char *read_line(char *line)
+{
     imprimir_prompt();
 
-    //leer linea
-    char *ret = fgets(line, lineSize, stdin);
-    //si es diferente de NULL o que empieze por #
-    if(ret && *ret!='#'){
-        char *pos=memchr(line,'\n',strlen(line));
-        *pos='\0';
-    }else if(ret == NULL){/*si NULL o EOF*/
+    // leer linea
+    char *ret = fgets(line, COMMAND_LINE_SIZE, stdin);
+    // si es diferente de NULL o que empieze por #
+    if (ret && *ret != '#')
+    {
+        char *pos = memchr(line, '\n', strlen(line));
+        *pos = '\0';
+    }
+    else if (ret == NULL)
+    { /*si NULL o EOF*/
         printf("\n");
-        if(feof(stdin)){/*se ha pulsado CTRL+D*/
-            fprintf(stderr,VERDE_T"BYE SIS\n"RESET);
+        if (feof(stdin))
+        { /*se ha pulsado CTRL+D*/
+            fprintf(stderr, VERDE_T "BYE SIS\n" RESET);
             exit(0);
         }
-    }else{
+    }
+    else
+    {
         ret = NULL;
     }
     return ret;
+}
+int parse_args(char **args, char *line)
+{
+    char *tok;
+    int contTok = 0;
 
+    tok = strtok(line, " \t\n");
+    if (tok = NULL)
+    {
+        args[contTok] = NULL;
+        return -1;
+    }
+    args[contTok] = tok;
+    contTok++;
+    // bucle de lectura de todos los tokens de la linea
+    while (tok != NULL)
+    {
+        args[contTok] = tok;
+        contTok++;
+        // Obtenemos el siguiente token
+        tok = strtok(NULL, " \t\n");
+    }
+    args[contTok] = NULL;
+    for (int j = 0; j < contTok; j++)
+    {
+        if ((char)*args[j] == '#')
+        {
+            fprintf(stderr, GRIS_T "[parse_args()→ token %d: %s]\n" RESET, j, args[j]);
+            args[j] = NULL;
+            fprintf(stderr, GRIS_T "[parse_args()→ token %d corregido: %s]\n" RESET, j, args[j]);
+            return -1;
+        }
+        else
+        {
+            fprintf(stderr, GRIS_T "[parse_args()→ token %d: %s]\n" RESET, j, args[j]);
+        }
+    }
+
+    fprintf(stderr, GRIS_T "[parse_args()→ token %d: %s]\n" RESET, contTok, args[contTok]);
+    return contTok;
 }
